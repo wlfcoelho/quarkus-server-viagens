00a1375033df88c5c7c2dc0f9c992e597384a06d
Wilson <wlfcoelho@gmail.com>
2026-07-04 12:50:57 -0300

    Ajustando configuração do módulo, renomeando Tools e aumentando o limite de texto do log

--- mcp-booking-server/src/main/java/dev/ia/travel/BookingTools.java
+++ mcp-booking-server/src/main/java/dev/ia/travel/BookingTools.java
@@ -11,8 +11,8 @@ import java.util.List;
 public class BookingTools {
     @Inject
     BookingService bookingService;
-
-    @Tool(name = "Obtém os detalhes completos de uma reserva com base em seu número de identificação (bookingId).")
+    //Cuidado com os nomes das Tools, pois, elas podem causar alucinação ma IA
+    @Tool(name = "get_booking_details")
     public String getBookingDetails(
             @ToolArg(description = "O ID numérico único da reserva (ex: 12345)") long bookingId) {
         return bookingService.getBookingDetails(bookingId)
@@ -20,10 +20,8 @@ public class BookingTools {
                 .orElse("Reserva com ID " + bookingId + " não encontrada.");
     }
 
-    @Tool(name = """
-            Cancela uma reserva existente com base no seu ID (bookingId).
-            O usuário deve estar autenticado.
-            """)
+    @Tool(
+            name = "cancel_booking")
     public String cancelBooking(
             @ToolArg(description = "ID da reserva a cancelar") long bookingId,
             @ToolArg(description = "Usuário que está tentando cancelar a reserva") String name) {
@@ -32,7 +30,7 @@ public class BookingTools {
                 .orElse("Não foi possível cancelar a reserva. Verifique se o ID está correto e se você tem permissão.");
     }
 
-    @Tool(name = "Lista os pacotes de viagem disponíveis para uma determinada categoria (ex: ADVENTURE, TREASURES).")
+    @Tool(name = "list_packages_by_category")
     public String listPackagesByCategory(
             @ToolArg(description = "Categoria utilizada como filtro para pacotes") Category category) {
         List<Booking> packages = bookingService.findPackagesByCategory(category);
